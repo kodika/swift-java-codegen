d266a37821cc80d4ad92809d9df7a4300e15daf6
Kostas Antonopoulos <kostas@devpro.gr>
2021-05-10 12:19:00 +0300

    [FIX] Window.swift in sample project

--- sample/src/main/swift/Sources/SampleAppCore/Window.swift
+++ sample/src/main/swift/Sources/SampleAppCore/Window.swift
@@ -3,6 +3,32 @@ import java_swift
 
 open class Window: JavaObject{
 
+    static public func from(javaObject: jobject) throws -> Window {
+        return Window(javaObject: javaObject)
+    }
+
+    // Create java object with native pointer
+    public func javaObject() throws -> jobject {
+        fatalError("Should not be needed")
+    }
+
+    public func release() {
+        Unmanaged.passUnretained(self).release()
+    }
+
+    public func retain() {
+        _ = Unmanaged.passUnretained(self).retain()
+    }
+
+    public var jniObject: jobject{
+        get{
+            if self.javaObject == nil{
+                self.javaObject = try! javaObject()
+            }
+            return self.javaObject!
+        }
+    }
+
     public func returnString(_ result: String?){
 
     }
